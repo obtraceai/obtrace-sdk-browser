55f8028caf7f8ad7e9b2db7dc1520e06a89a17bc
cendy Andreoli <cendyandreoli@gmail.com>
2026-03-29 01:52:54 -0300

    v1.2.0: init handshake, clean publish

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -229,7 +229,7 @@ export function initBrowserSDK(config: ObtraceSDKConfig): BrowserSDK {
     },
     body: JSON.stringify({
       sdk: "obtrace-sdk-browser",
-      sdk_version: "1.0.2",
+      sdk_version: "1.2.0",
       service_name: config.serviceName,
       service_version: config.serviceVersion ?? "",
       runtime: "browser",
